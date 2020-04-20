9bef25384682a8beec3c0df82f7bc8c17df05056
Uriel Sánchez <17171295@itculiacan.edu.mx>
2020-04-20 01:25:54 -0600

    Updates

---
+++ precache-manifest.448a09df04a594dba2e3f1dedc8de09b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "138f9fff9c53f4ba823c86dd4c9c0dcf",
+    "url": "/reactnotes/index.html"
+  },
+  {
+    "revision": "73f2c5d75c51f80bf41e",
+    "url": "/reactnotes/static/css/main.7a108bc3.chunk.css"
+  },
+  {
+    "revision": "ca4b84382c4c8c2a581a",
+    "url": "/reactnotes/static/js/2.d8425ce8.chunk.js"
+  },
+  {
+    "revision": "faec59650cea46a85a5e563bc9c613f8",
+    "url": "/reactnotes/static/js/2.d8425ce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73f2c5d75c51f80bf41e",
+    "url": "/reactnotes/static/js/main.c550fd61.chunk.js"
+  },
+  {
+    "revision": "389f823f1e982e6e59bc",
+    "url": "/reactnotes/static/js/runtime-main.713b543d.js"
+  }
+]);
